d08175c6f1a05560c0ce813ead4b20af0048c9f8
Jeffery Phipps <jefferyphipps@att.net>
2024-07-11 10:58:22 -0500

    Finished Assignment 2: Scrabble Scorer with bonus tasks

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -3,6 +3,7 @@
 const input = require("readline-sync");
 
 const oldPointStructure = {
+   //0: [' '],
   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
   2: ['D', 'G'],
   3: ['B', 'C', 'M', 'P'],
@@ -33,25 +34,138 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let scrabbleWord = input.question("Let's play some scrabble! Enter a word:").toLowerCase();
+   
+   while(test (scrabbleWord)){
+      scrabbleWord = input.question("Please enter a valid word. Enter a word:").toLowerCase();
+   }
+
+   return scrabbleWord;
+}
+
+function test(word) {
+   let flag = false;
+   let checkWord = word.split('');
+   for (let x = 0; x<checkWord.length; x++){
+      if(!(checkWord[x] in newPointStructure)){//this isnt a word
+         console.log("This has a character that is not a letter in it.")
+         flag = true;
+      } 
+   }
+   return flag;
+}
+
+let newPointStructure = transform(oldPointStructure);
+newPointStructure[' '] = 0;
+
+let simpleScorer = function(word){
+   return word.length;
 };
 
-let newPointStructure;
+let vowelBonusScorer = function(word){
+   let arr = word.split('');
+   let score = 0;
+   for (let x = 0; x<arr.length;x++){
+      if(arr[x]==='a')
+         score +=3;
+         else if (arr[x]==='e')
+            score +=3;
+            else if (arr[x]==='i')
+               score +=3;
+               else if (arr[x]==='o')
+                  score +=3;
+                  else if (arr[x]==='u')
+                     score +=3;
+                     else
+                        score +=1;
+   }
+   return score;
+};
 
-let simpleScorer;
+let scrabbleScorer = function (word){
+   word = word.toLowerCase().split('');
+	let score = 0;
+   
+	for (let x = 0; x < word.length; x++) {
+      score += newPointStructure[word[x]];
+	}
+	return score;
+};
 
-let vowelBonusScorer;
+const scoringAlgorithms = [{
+      name: "Simple Score",
+      description : "Each letter is worth 1 point.",
+      scorerFunction: simpleScorer
+   }, 
+   {
+      name: "Bonus Vowels",
+      description: "Vowels are 3 pts, consonants are 1 pt.",
+      scorerFunction: vowelBonusScorer
+   }, 
+   {
+      name: "Scrabble",
+      description: "The traditional scoring algorithm.",
+      scorerFunction: scrabbleScorer
+   }];
 
-let scrabbleScorer;
+function scorerPrompt() {
+   console.log("What type of scoring algorithm would you like to use?");
+   console.log( `0 is ${scoringAlgorithms[0].name} and it's description is ${scoringAlgorithms[0].description}`);
+   console.log( `1 is ${scoringAlgorithms[1].name} and it's description is ${scoringAlgorithms[1].description}`);
+   console.log( `2 is ${scoringAlgorithms[2].name} and it's description is ${scoringAlgorithms[2].description}`);
+   let selection = -1;
+   while(selection<0 || selection>2){
+      selection = input.question("Please select between 0, 1, or 2. ");
+      if (selection<0 || selection>2)
+         console.log("Please choose 0, 1, or 2.");
+   }
+   return scoringAlgorithms[selection];
+}
 
-const scoringAlgorithms = [];
-
-function scorerPrompt() {}
-
-function transform() {};
+function transform(oldPoints) {
+   let newObject = {};
+   let counter = 0;
+   for(number in oldPoints){
+      
+      for(let x = 0; x<oldPoints[number].length; x++){
+      
+         let letter = oldPoints[number][x].toLowerCase();
+         counter ++;
+         newObject[letter] = Number(number);
+     
+         //console.log(letter + " "+  newObject[letter] + " "+ counter );
+      }
+   }
+    //console.log(newObject);
+   return newObject;
+};
 
 function runProgram() {
-   initialPrompt();
+   let scrabWord = initialPrompt();
+   let scoringAlgorithm = scorerPrompt();
+   let keepPlaying = true;
+   console.log("You choose this scoring method: ", scoringAlgorithm.name);
+   console.log(`Value for the word ${scrabWord}`, scoringAlgorithm.scorerFunction(scrabWord));
+   while(keepPlaying){
+      console.log("Would you like to keep playing?");
+      let choice = input.question("Please choose yes or no: ").toLowerCase();
+      if (choice === 'yes'|| choice === 'no'){
+         if(choice === 'yes'){
+            scrabWord = initialPrompt();
+            scoringAlgorithm = scorerPrompt();
+            console.log("You choose this scoring method: ", scoringAlgorithm.name);
+            console.log(`Value for the word ${scrabWord}`, scoringAlgorithm.scorerFunction(scrabWord));
+         }else{
+            keepPlaying = false;
+            console.log("Thank you for playing!");
+         }
+            
+      }else{
+         console.log("That was not a valid choice. Please choose yes or no.")
+      }
+   }
+   
+
    
 }
 
